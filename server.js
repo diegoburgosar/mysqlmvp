e4cddceb65474e0696c5203d1a0c743d62cc22a2
Admin <admin@Zuquitos-Mac.fibertel.com.ar>
2019-11-21 16:10:38 -0300

    comment del error

--- server.js
+++ server.js
@@ -2,27 +2,28 @@ const mysql = require ("mysql");
 const express = require ("express");
 const bodyParse = require ("body-parser");
 
- 
 var app = express();
 app.use(bodyParse.json());
  
-
-
 var mysqlConnection = mysql.createConnection({
+    
+        host:"35.222.231.62",
+        user:"root", 
+        port:"3306", 
+        database:"PODroot2019",
+        multipleStatements:true
+/*
         host:"localhost",
         user:"root", 
+        port:"3306",
         password:"PODroot2019",
         database:"early_adopters",
         multipleStatements:true
+        */
 });
 
 mysqlConnection.connect((err)=> {
     console.log("=============================");
-    //console.log("=>>>>>",err );
-   
+    console.log("=>>>>>",err );
 });
-
-
-
-
 app.listen(3000);
